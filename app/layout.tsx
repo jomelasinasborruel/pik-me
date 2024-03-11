109c4b12dacb7e262e2d8478356b22b023f9550f
jomelasinasborruel <jomel.borruel@oqulo.com>
2024-03-12 01:00:37 +0800

    FIX: Tab name

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "@/public/fonts/runcort/stylesheet.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Pic-me",
+  title: "Pik-me",
   description: "Mini project",
 };
 
