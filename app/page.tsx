9098b46bbff56920fa204b1e710efe3032fbe82d
jomelasinasborruel <jomel.borruel@oqulo.com>
2024-03-17 01:06:47 +0800

    UPDATE: Background music control, game reset, and classes

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,8 @@ import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
 import { useMediaQuery } from "usehooks-ts";
 import useCustomTimer from "@/lib/timer";
 import Frame from "@/components/Frame";
-// import { useSearchParams } from "next/navigation";
+import cx from "./Home.module.scss";
+import { BiVolumeFull, BiVolumeMute } from "react-icons/bi";
 
 export default function Home() {
   const [opaque, setOpaque] = React.useState<boolean>();
@@ -18,10 +19,10 @@ export default function Home() {
   const [soundApplause, setSoundApplause] = React.useState<HTMLAudioElement>();
   const isDesktop = useMediaQuery("(min-width: 800px)");
 
-  // const searchParams = useSearchParams();
   const [pieces, setPieces] = React.useState<DEFAULT_PIECES_PROPS[]>();
   const [isSuccess, setIsSuccess] = React.useState(false);
   const [numberOfMoves, setNumberOfMoves] = React.useState(0);
+  const [bgMusicOn, setBgMusicOn] = React.useState(true);
 
   const { start, pause, reset, seconds, miliseconds, minutes } =
     useCustomTimer();
@@ -29,13 +30,17 @@ export default function Home() {
 
   const handleStart = () => {
     setOpaque(false);
+    setNumberOfMoves(0);
+    setIsSuccess(false);
+    reset();
     let jumbledSet: number[] = [];
 
     // if (searchParams.get("unbeatable") == "true") {
     //   jumbledSet = [0, 1, 2, 3, 4, 5, 7, 6, 8];
     // } else {
-      jumbledSet = [7, 6, 3, 8, 2, 4, 5, 1, 0];
     // }
+    // jumbledSet = [7, 6, 3, 8, 2, 4, 5, 1, 0];
+    jumbledSet = [0, 1, 2, 3, 4, 8, 6, 7, 5];
 
     const initPieces = pieces;
 
@@ -58,6 +63,29 @@ export default function Home() {
     }, 200);
   };
 
+  const handleResetGame = () => {
+    const size = isDesktop
+      ? { width: 500, height: 625 }
+      : { width: 320, height: 400 };
+    setFrameSize(size);
+
+    setPieces(DEFAULT_PIECES(size.width, size.height));
+    setOpaque(undefined);
+    setNumberOfMoves(0);
+    setIsSuccess(false);
+    reset();
+  };
+
+  const handleAudio = () => {
+    if (bgMusicOn) {
+      setBgMusicOn(false);
+      bgMusic?.pause();
+    } else {
+      setBgMusicOn(true);
+      bgMusic?.play();
+    }
+  };
+
   useEffect(() => {
     if (!pieces) return;
     const isSame = pieces.filter((pc, index) => pc.position !== index);
@@ -69,15 +97,15 @@ export default function Home() {
   }, [pieces]);
 
   useEffect(() => {
-    const size = isDesktop
-      ? { width: 500, height: 625 }
-      : { width: 320, height: 400 };
-    setFrameSize(size);
+    if (!isSuccess) return;
+    delay(() => {
+      setOpaque(undefined);
+      setIsSuccess(false);
+    }, 6000);
+  }, [isSuccess]);
 
-    setPieces(DEFAULT_PIECES(size.width, size.height));
-    setOpaque(undefined);
-    setNumberOfMoves(0);
-    reset();
+  useEffect(() => {
+    handleResetGame();
   }, [isDesktop]);
 
   useEffect(() => {
@@ -86,29 +114,33 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex relative min-h-screen flex-col items-center justify-center select-none">
-      <div
-        className={clsx(
-          "transition-all opacity-100 duration-500 absolute w-full h-screen flex-col gap-4 z-50 left-0 top-0 bg-[#202020] flex justify-center items-center",
-          {
-            "!opacity-0 !pointer-events-none": !isFirstTime,
-          }
+    <main className={cx["main"]}>
+      <button
+        className="absolute z-50 right-2 top-2 text-white"
+        onClick={handleAudio}
+      >
+        {bgMusicOn ? (
+          <BiVolumeFull className="text-[2rem]" />
+        ) : (
+          <BiVolumeMute className="text-[2rem]" />
         )}
+      </button>
+      <div
+        className={clsx(cx["cover"], {
+          "!opacity-0 !pointer-events-none": !isFirstTime,
+        })}
       >
         <Image
           width={500}
           height={190}
-          className="!w-[300px] !h-auto transition-all duration-500"
+          className={cx["cover__logo"]}
           alt={""}
           src={"/images/svgs/logo.svg"}
         />
         <button
-          className={clsx(
-            " font-runcort text-[#dbdbdb] text-3xl transition-color duration-500 rounded-md py-2 px-4 bg-[#204311] z-20 hover:bg-[#284c19] hover:text-white",
-            {
-              hidden: opaque !== undefined,
-            }
-          )}
+          className={clsx(cx["cover__button"], {
+            hidden: opaque !== undefined,
+          })}
           onClick={() => {
             setIsFirstTime(false);
             Cookies.set("pm-isFirstTime", "false");
@@ -121,7 +153,7 @@ export default function Home() {
         </button>
       </div>
 
-      <div className="absolute w-full h-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className={cx["main__bg"]}>
         <Image
           fill
           draggable={false}
@@ -131,73 +163,57 @@ export default function Home() {
         />
         <div className="absolute top-0 left-0 w-full h-full bg-[#00000079]" />
       </div>
-      <div className="relative tracking-widest drop-shadow-2xl mt-4">
+      <div className={cx["main__logo-container"]}>
         <Image
           width={500}
           height={190}
-          className="!w-[300px] !h-auto"
+          className={cx["main__logo"]}
           alt={""}
           src={"/images/svgs/logo.svg"}
         />
       </div>
-      <div className="flex p-4 min-[515px]:h-full max-[514px]:flex-col max-w-[1080px] w-full justify-center items-center gap-4">
+      <div className={cx["frame-container"]}>
         <div
-          className={clsx(
-            "w-[320px] transition-all flex max-[514px]:flex-row flex-col min-[515px]:gap-4 min-[515px]:w-[150px] min-[515px]:h-[400px] min-[800px]:w-[250px] min-[800px]:h-[625px] duration-300 h-[200px] relative border-[0.3125rem] bg-[#202020] border-[#202020] rounded-lg  overflow-hidden",
-            {
-              "!border-green-900 ": isSuccess,
-            }
-          )}
+          className={clsx(cx["infos"], {
+            "!border-green-900 ": isSuccess,
+          })}
         >
-          <div>
-            <Image
-              fill
-              draggable={false}
-              className="object-cover !w-full max-[514px]:!min-w-[152px] max-[514px]:!h-[190px] !h-auto !relative"
-              alt="original"
-              src={"/images/puzzles/pikachu.jpg"}
-            />
-          </div>
-          <div className="h-full font-runcort tracking-wider text-[#ffffffb1] w-full flex flex-col gap-2 min-[800px]:gap-4 p-2">
+          <Image
+            fill
+            draggable={false}
+            className={cx["original-photo"]}
+            alt="original"
+            src={"/images/puzzles/pikachu.jpg"}
+          />
+
+          <div className={cx["game-details"]}>
             <p>
               Elapse time: <br />
             </p>
 
-            <p className="text-center text-2xl min-[800px]:text-3xl">
+            <p className={cx["score--time"]}>
               {minutes}:{seconds}:{miliseconds}
             </p>
 
             <p>Moves:</p>
-            <p className="text-center text-2xl min-[800px]:text-3xl">
-              {" "}
-              {numberOfMoves}
-            </p>
+            <p className={cx["score--move"]}> {numberOfMoves}</p>
           </div>
         </div>
         <div
           style={{ width: frameSize?.width, height: frameSize?.height }}
-          className={clsx(
-            "flex relative transition-all duration-300 border-[0.3125rem] border-[#202020] bg-[#202020] rounded-lg overflow-hidden",
-            {
-              "!border-green-900 !bg-green-900 !pointer-events-none": isSuccess,
-            }
-          )}
+          className={clsx(cx["frame"], {
+            "!border-green-900 !bg-green-900 ": isSuccess,
+          })}
         >
           <div
-            className={clsx(
-              "absolute top-0 left-0 w-full h-full bg-[#00000079] z-10",
-              {
-                hidden: opaque,
-              }
-            )}
+            className={clsx(cx["frame-overlay"], {
+              hidden: opaque && !isSuccess,
+            })}
           />
           <button
-            className={clsx(
-              "absolute top-1/2 left-1/2 font-runcort text-[#dbdbdb] -translate-x-1/2 text-3xl transition-color duration-500 -translate-y-1/2 rounded-md py-2 px-4 bg-[#204311] z-20 hover:bg-[#284c19] hover:text-white",
-              {
-                hidden: opaque !== undefined,
-              }
-            )}
+            className={clsx(cx["frame__button"], {
+              hidden: opaque !== undefined,
+            })}
             onClick={handleStart}
           >
             START
@@ -216,9 +232,9 @@ export default function Home() {
           )}
         </div>
       </div>
-      {isSuccess && (
-        <div className="absolute top-0 left-0 w-full h-full">
-          <Fireworks autorun={{ speed: 1, duration: 6000 }} />
+      {isSuccess && opaque && (
+        <div className={cx["win-effect"]}>
+          <Fireworks autorun={{ speed: 1, duration: 5000 }} />
         </div>
       )}
     </main>
